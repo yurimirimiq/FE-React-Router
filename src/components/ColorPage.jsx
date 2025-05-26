db99ca9d1fe7ed4665715ed79b72eafe5c37de1c
강유림 <yurimirimiq@gmail.com>
2025-05-26 20:20:14 +0900

    fff

--- src/components/ColorPage.jsx
+++ src/components/ColorPage.jsx
@@ -2,10 +2,22 @@ import { useParams } from 'react-router-dom';
 import '../styles/ColorPage.css';
 
 function ColorPage() {
+  const { id } = useParams(); // URL에서 색상 이름 추출
+
+  const pageStyle = {
+    backgroundColor: id,
+    height: '200vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: '#fff',
+    fontSize: '2rem',
+    flexDirection: 'column',
+  };
 
   return (
-    <div className="color-page">
-    배경 페이지입니다 
+    <div style={pageStyle}>
+      <p>이 페이지는 <strong>{id}</strong> 색상입니다 🎨</p>
     </div>
   );
 }
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,14 +1,37 @@
 import { useNavigate } from 'react-router-dom';
 import '../styles/MainPage.css';
 
-const Mainpage = () => {
- 
+const MainPage = () => {
+  const navigate = useNavigate();
+
+  const colors = ['red', 'blue', 'orange', 'pink', 'yellow'];
+
   return (
     <div className="container">
       <h2>React 색상 선택</h2>
       <hr />
+      <div className="color-buttons">
+        {colors.map((color) => (
+          <button
+            key={color}
+            style={{
+              backgroundColor: color,
+              color: color === 'yellow' ? 'black' : 'white',
+              margin: '5px',
+              padding: '10px 0px',
+              border: 'none',
+              borderRadius: '5px',
+              cursor: 'pointer',
+              fontSize: '1rem',
+            }}
+            onClick={() => navigate(`/color/${color}`)}
+          >
+            {color}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
 
-export default Mainpage;
+export default MainPage;
